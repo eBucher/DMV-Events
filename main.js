1448dd5a29d3867fb92ea50649a70893597dd9e4
= <erichdbucher@gmail.com>
2025-02-02 15:55:08 -0500

    Add farmers market data

--- main.js
+++ main.js
@@ -52,6 +52,32 @@ function pill_for_category(category) {
 
 }
 
+// Name should be the name of an event that contains at least one day of the week in it
+function pills_for_day_of_week(name) {
+
+    var pill_template = '<span class="badge rounded-pill text-bg-COLOR">TEXT</span>';
+    var return_string = "";
+
+    var pill_text = "";
+    if(name.includes("Monday")) {
+        return_string += pill_template.replace("COLOR", "dark").replace("TEXT", "Monday");
+    } if(name.includes("Tuesday")) {
+        return_string += pill_template.replace("COLOR", "secondary").replace("TEXT", "Tuesday");
+    } if(name.includes("Wednesday")) {
+        return_string += pill_template.replace("COLOR", "warning").replace("TEXT", "Wednesday");
+    } if(name.includes("Thursday")) {
+        return_string += pill_template.replace("COLOR", "danger").replace("TEXT", "Thursday");
+    } if(name.includes("Friday")) {
+        return_string += pill_template.replace("COLOR", "info").replace("TEXT", "Friday");
+    } if(name.includes("Saturday")) {
+        return_string += pill_template.replace("COLOR", "primary").replace("TEXT", "Saturday");
+    } if(name.includes("Sunday")) {
+        return_string += pill_template.replace("COLOR", "success").replace("TEXT", "Sunday");
+    }
+
+    return return_string;
+}
+
 function update_events_for_month() {
 
     if(document.getElementById("only_show_free_events_switch").checked == true) {
@@ -62,13 +88,15 @@ function update_events_for_month() {
         var filtered_events = events_by_month_data
     }
 
-    let events = {Annual:[], Sports:[]};
+    let events = {Annual:[], Sports:[], Farmers_Markets:[]};
 
     for(let i = 0; i < filtered_events.length; i++) {
         let event_obj = filtered_events[i];
         if(event_obj.Month == shown_month_name) {
             if(event_obj.Category == "Sports") {
                 events.Sports.push(event_obj);
+            } else if(event_obj.Category == "Farmer's Market") {
+                events.Farmers_Markets.push(event_obj);
             } else {
                 events.Annual.push(event_obj);
             }
@@ -101,6 +129,23 @@ function update_events_for_month() {
         new_html += '<ul>';
         for(let i = 0; i < events.Sports.length; i++) {
             new_html += '<li><a target = "_blank" href="' + events.Sports[i].Link + '">' + " " + events.Sports[i].Name + '</a></li>'
+        }
+        new_html += "</ul>";
+    }
+
+    insertion_div.innerHTML = new_html;
+
+
+    // Insert Farmer's Markets
+    insertion_div = document.getElementById("shown_farmers_markets");
+    new_html = ""
+
+    if(events.Farmers_Markets.length == 0) {
+        new_html += "None<br>"
+    } else {
+        new_html += '<ul>';
+        for(let i = 0; i < events.Farmers_Markets.length; i++) {
+            new_html += '<li><a target = "_blank" href="' + events.Farmers_Markets[i].Link + '">' + pills_for_day_of_week(events.Farmers_Markets[i].Name) + " " + events.Farmers_Markets[i].Name + '</a></li>'
         }
         new_html += "</ul>";
     }
